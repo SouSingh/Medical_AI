2cde13ec59cfffe91846c6e67bda560b4d85e12a
Sourabh singh <96938361+SouSingh@users.noreply.github.com>
2024-03-13 22:06:26 +0530

    Update code.py

--- code.py
+++ code.py
@@ -7,8 +7,10 @@ from llama_index.core import SimpleDirectoryReader
 from llama_index.core.node_parser import SimpleFileNodeParser
 from llama_index.vector_stores.weaviate import WeaviateVectorStore
 from llama_index.core import VectorStoreIndex, StorageContext
+from llama_index.core.node_parser import SentenceSplitter
 import weaviate
 import uvicorn
+from llama_index.core import Settings
 
 load_dotenv()
 
@@ -16,8 +18,7 @@ app = FastAPI()
 
 api_key = os.environ.get('OPENAI_API_KEY')
 openai.api_key = api_key
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+os.getenv("OPENAI_API_KEY")
 WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
 WEAVIATE_URL = os.getenv("WEAVIATE_URL")
 auth_config = weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY)
@@ -28,15 +29,18 @@ client = weaviate.Client(
 )
 
 
+
 def search_and_query():
     blogs = SimpleDirectoryReader("./Data").load_data()
-    vector_store = WeaviateVectorStore(weaviate_client=client, index_name="NABH")
+    text_splitter = SentenceSplitter(chunk_size=512, chunk_overlap=10)
+    Settings.text_splitter = text_splitter
+    vector_store = WeaviateVectorStore(weaviate_client=client, index_name="NABSH")
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
-    VectorStoreIndex.from_documents(blogs, storage_context=storage_context)
+    VectorStoreIndex.from_documents(blogs, storage_context=storage_context, transformations=[text_splitter])
     return "Done Embeddings"
 
 def Quert(ask):
-    vector_store = WeaviateVectorStore(weaviate_client=client, index_name="NABH")
+    vector_store = WeaviateVectorStore(weaviate_client=client, index_name="NABSH")
     loaded_index = VectorStoreIndex.from_vector_store(vector_store)
     query_engine = loaded_index.as_query_engine()
     response = query_engine.query(ask)
@@ -56,14 +60,14 @@ def contract_analysis_w_fact_checking(text):
     if quert_instance.response:
         contract_results = [{
             "LLM Response": quert_instance.response,
-            "Source_node": {
-                "Page_number": quert_instance.source_nodes[0].node.metadata.get('page_label', ''),
-                "File_Name": quert_instance.source_nodes[0].node.metadata.get('file_name', ''),
-                "Text": quert_instance.source_nodes[0].node.text,
-                "Start_Char": quert_instance.source_nodes[0].node.start_char_idx,
-                "End_Char": quert_instance.source_nodes[0].node.end_char_idx,
-                "Score_Matching": quert_instance.source_nodes[0].score
-            }
+            "Source_node": [{
+                "Page_number": nexted.node.metadata.get('page_label', ''),
+                "File_Name": nexted.node.metadata.get('file_name', ''),
+                "Text": nexted.node.text,
+                "Start_Char": nexted.node.start_char_idx,
+                "End_Char": nexted.node.end_char_idx,
+                "Score_Matching": nexted.score}
+                for nexted in quert_instance.source_nodes]
         }]
     else:
         contract_results = []
